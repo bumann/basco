626738a2dc3e1f5798d988c2c991d629c805645c
bumann <bumann@gmx.ch>
2014-06-05 06:33:27 +0200

    DriverModule fixed

--- Basco.Samples/Driver/DriverModule.cs
+++ Basco.Samples/Driver/DriverModule.cs
@@ -8,7 +8,7 @@
         public override void Load()
         {
             this.Bind<IDriver>().To<Driver>()
-                .DefinesNamedScope(DriverConstants.DriverScope);
+                .DefinesNamedScope(Driver.DriverScope);
         }
     }
 }
