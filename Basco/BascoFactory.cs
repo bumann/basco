75c0c8d33b5ee926f1f9f6f3f2677796e372657d
bumann <bumann@gmx.ch>
2014-07-03 06:58:30 +0200

    basco factory added (when no dependency injection is used)

--- Basco/BascoFactory.cs
+++ Basco/BascoFactory.cs
@@ -1,7 +1,11 @@
 ﻿namespace Basco
 {
     using System;
+    using Scyano;
 
+    /// <summary>
+    /// Factory to create IBasco if no dependency injection (like Ninject, ...) is used.
+    /// </summary>
     public class BascoFactory
     {
         public static IBasco<TTrigger> Create<TTrigger>(IBascoStatesFactory statesFactory, IBascoConfigurator<TTrigger> bascoConfigurator)
