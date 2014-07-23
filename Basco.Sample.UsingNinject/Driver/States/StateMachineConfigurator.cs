40030f2377caaa6421a34afe06c693150713b08c
bumann <bumann@gmx.ch>
2014-07-23 07:32:53 +0200

    adapt ninject sample

--- Basco.Sample.UsingNinject/Driver/States/StateMachineConfigurator.cs
+++ Basco.Sample.UsingNinject/Driver/States/StateMachineConfigurator.cs
@@ -1,22 +1,20 @@
 ﻿namespace Basco.Sample.UsingNinject.Driver.States
 {
+    using Basco.Configuration;
+
     public class StateMachineConfigurator : IBascoConfigurator<TransitionTrigger>
     {
         public void Configurate(IBasco<TransitionTrigger> basco)
         {
-            basco.In<ConnectedState, TransitionTrigger>()
-                .On(TransitionTrigger.Run).Goto<ProcessingState, TransitionTrigger>()
-                .On(TransitionTrigger.Stop).ReEnter()
-                .On(TransitionTrigger.Error).Stay()
-                .On(TransitionTrigger.Reset).Stay();
+            basco.In<ConnectedState, TransitionTrigger>(x => x
+                .When(TransitionTrigger.Run).Goto<ProcessingState>());
 
-            basco.In<ProcessingState, TransitionTrigger>()
-                .On(TransitionTrigger.Stop).Goto<ConnectedState, TransitionTrigger>()
-                .On(TransitionTrigger.Error).Goto<ErrorState, TransitionTrigger>()
-                .On(TransitionTrigger.Reset).Stay();
+            basco.In<ProcessingState, TransitionTrigger>(x => x
+                .When(TransitionTrigger.Stop).Goto<ConnectedState>()
+                .When(TransitionTrigger.Error).Goto<ErrorState>());
 
-            basco.In<ErrorState, TransitionTrigger>()
-                .On(TransitionTrigger.Reset).Goto<ConnectedState, TransitionTrigger>();
+            basco.In<ErrorState, TransitionTrigger>(x => x
+                .When(TransitionTrigger.Reset).Goto<ConnectedState>());
         }
     }
 }
