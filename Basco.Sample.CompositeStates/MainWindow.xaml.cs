c42cae84249f4abd4b13d6d7f31dfc10cec9cff4
bumann <bumann@gmx.ch>
2014-08-14 16:18:04 +0200

    two more controls added

--- Basco.Sample.CompositeStates/MainWindow.xaml.cs
+++ Basco.Sample.CompositeStates/MainWindow.xaml.cs
@@ -12,11 +12,15 @@
 
             var driverViewModel1 = kernel.Get<IDriverControlModel>();
             var driverViewModel2 = kernel.Get<IDriverControlModel>();
+            var driverViewModel3 = kernel.Get<IDriverControlModel>();
+            var driverViewModel4 = kernel.Get<IDriverControlModel>();
 
             this.InitializeComponent();
 
             this.DriverOne.DataContext = driverViewModel1;
             this.DriverTwo.DataContext = driverViewModel2;
+            this.DriverThree.DataContext = driverViewModel3;
+            this.DriverFour.DataContext = driverViewModel4;
         }
     }
 }
