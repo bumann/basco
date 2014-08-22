25be5e5c695aaee95e21c4160b2ae424d0b27fa4
bumann <bumann@gmx.ch>
2014-08-22 06:41:06 +0200

    summary for BascoExtensions

--- Basco/Configuration/BascoExtensions.cs
+++ Basco/Configuration/BascoExtensions.cs
@@ -4,6 +4,14 @@
 
     public static class BascoExtensions
     {
+        /// <summary>
+        /// Extension to configure a state of the 'Basco' hierarchical state machine.
+        /// </summary>
+        /// <typeparam name="TState">The state (IState) that is configured.</typeparam>
+        /// <typeparam name="TTransitionTrigger">The enum defining all transitions in the state machine.</typeparam>
+        /// <param name="basco">The hierarchical state machine to configure.</param>
+        /// <param name="configurator">Action to configure the transitions of the state.</param>
+        /// <returns>The hierarchical state machine for fluent configuration.</returns>
         public static IBascoInternal<TTransitionTrigger> For<TState, TTransitionTrigger>(this IBascoInternal<TTransitionTrigger> basco, Action<ITransitionConfigurator<TTransitionTrigger>> configurator)
             where TState : class, IState
             where TTransitionTrigger : IComparable
@@ -14,6 +22,14 @@
             return basco;
         }
 
+        /// <summary>
+        /// Extension to configure a composite state of the 'Basco' hierarchical state machine.
+        /// </summary>
+        /// <typeparam name="TState">The composite state (IState) that is configured.</typeparam>
+        /// <typeparam name="TTransitionTrigger">The enum defining all transitions in the state machine.</typeparam>
+        /// <param name="basco">The hierarchical state machine to configure.</param>
+        /// <param name="configurator">Action to configure the transitions of the composite state.</param>
+        /// <returns>The hierarchical state machine for fluent configuration.</returns>
         public static IBascoInternal<TTransitionTrigger> ForComposite<TState, TTransitionTrigger>(this IBascoInternal<TTransitionTrigger> basco, Action<ITransitionConfigurator<TTransitionTrigger>> configurator)
             where TState : class, IState
             where TTransitionTrigger : IComparable
