ce11fd51d0b7718264bc4a580d329871460aee50
bumann <bumann@gmx.ch>
2014-08-28 07:51:26 +0200

    tests added

--- Basco.Test/Configuration/BascoExtensionsTest.cs
+++ Basco.Test/Configuration/BascoExtensionsTest.cs
@@ -10,7 +10,7 @@
     public class BascoExtensionsTest
     {
         [Fact]
-        public void In_MustReturnBasco()
+        public void For_MustReturnBasco()
         {
             var basco = new Basco<TestTrigger>(
                 Mock.Of<IScyano>(), 
@@ -25,7 +25,7 @@
         }
 
         [Fact]
-        public void In_MustAddStateTransitionToExecutor()
+        public void For_MustAddStateTransitionToExecutor()
         {
             var transitionCache = new Mock<IBascoTransitionCache<TestTrigger>>();
             var basco = new Basco<TestTrigger>(
@@ -36,6 +36,37 @@
                 Mock.Of<IBascoConfigurator<TestTrigger>>());
 
             basco.For<SimpleTestState, TestTrigger>(configurator => { });
+
+            transitionCache.Verify(x => x.Add(typeof(SimpleTestState), It.IsAny<StateTransitions<TestTrigger>>()));
+        }
+
+        [Fact]
+        public void ForComposite_MustReturnBasco()
+        {
+            var basco = new Basco<TestTrigger>(
+                Mock.Of<IScyano>(),
+                Mock.Of<IBascoStateCache<TestTrigger>>(),
+                Mock.Of<IBascoTransitionCache<TestTrigger>>(),
+                Mock.Of<IBascoExecutor<TestTrigger>>(),
+                Mock.Of<IBascoConfigurator<TestTrigger>>());
+
+            IBasco<TestTrigger> result = basco.ForComposite<SimpleTestState, TestTrigger>(configurator => { });
+
+            result.Should().BeOfType<Basco<TestTrigger>>();
+        }
+
+        [Fact]
+        public void ForComposite_MustAddStateTransitionToExecutor()
+        {
+            var transitionCache = new Mock<IBascoTransitionCache<TestTrigger>>();
+            var basco = new Basco<TestTrigger>(
+                Mock.Of<IScyano>(),
+                Mock.Of<IBascoStateCache<TestTrigger>>(),
+                transitionCache.Object,
+                Mock.Of<IBascoExecutor<TestTrigger>>(),
+                Mock.Of<IBascoConfigurator<TestTrigger>>());
+
+            basco.ForComposite<SimpleTestState, TestTrigger>(configurator => { });
 
             transitionCache.Verify(x => x.Add(typeof(SimpleTestState), It.IsAny<StateTransitions<TestTrigger>>()));
         }
---
+++ Basco.Test/Configuration/StateConfiguratorTest.cs
@@ -0,0 +1,44 @@
+﻿namespace Basco.Test.Configuration
+{
+    using System;
+    using System.Linq;
+    using Basco.Configuration;
+    using Moq;
+    using Xunit;
+
+    public class StateConfiguratorTest
+    {
+        private readonly Mock<IBascoInternal<TestTrigger>> basco;
+        private readonly StateConfigurator<TestTrigger> testee;
+
+        public StateConfiguratorTest()
+        {
+            this.basco = new Mock<IBascoInternal<TestTrigger>>();
+            this.testee = new StateConfigurator<TestTrigger>(this.basco.Object);
+        }
+
+        [Fact]
+        public void ForState_MustAddTransitionCache()
+        {
+            var expectedType = typeof(SimpleTestState);
+
+            this.testee.ForState<SimpleTestState>();
+
+            this.basco.Verify(x => x.AddTransitionCache(expectedType, It.IsAny<StateTransitions<TestTrigger>>()));
+        }
+
+        [Fact]
+        public void StateConfigurator_MustConfigureTransitionCache()
+        {
+            this.testee.ForState<SimpleTestState>();
+            this.testee.When(TestTrigger.TransitionTwo);
+            this.testee.Goto<ExtendedTestState>();
+
+            this.basco.Verify(x => x.AddTransitionCache(
+                It.IsAny<Type>(), 
+                It.Is<StateTransitions<TestTrigger>>(t => 
+                    t.Transitions.Keys.Single() == TestTrigger.TransitionTwo 
+                    && t.Transitions.Values.Single() == typeof(ExtendedTestState))));
+        }
+    }
+}
